f59b78ee04bcfffeae859b7bfe5d8e9a341fbcac
Nilzao <nilzao@gmail.com>
2012-05-21 17:00:28 -0300

    exporter vereadores

--- app/exporter/bean/db/VereadoresVereancas.php
+++ app/exporter/bean/db/VereadoresVereancas.php
@@ -3,8 +3,8 @@ class app_exporter_bean_db_VereadoresVereancas {
 	public $id;
 	public $id_vereador;
 	public $id_vereador_anterior;
-	private $data_ini;
-	private $data_fim;
+	public $data_ini;
+	public $data_fim;
 	public $situacao;
 	public $partido;
 	public $partido_obs;
@@ -16,13 +16,5 @@ class app_exporter_bean_db_VereadoresVereancas {
 	
 	public function setDatafim($date){
 		$this->data_fim = app_importer_lib_FixDataToBr::fixData($date);
-	}
-	
-	public function getData_ini(){
-		return $this->data_ini;
-	}
-	
-	public function getData_fim(){
-		return $this->data_fim;
 	}
 }
--- app/exporter/domain/Vereadores.php
+++ app/exporter/domain/Vereadores.php
@@ -17,17 +17,41 @@ class app_exporter_domain_Vereadores {
 	
 	public function indexHandler(){
 		echo "export Vereadores\n";
+		$jsonStr = '';
 		$gabineteAoDb = new app_exporter_ao_db_Gabinetes();
 		$vereadorAoDb = new app_exporter_ao_db_Vereadores();
 		$vereadorVereancaAoDb = new app_exporter_ao_db_VereadoresVereancas();
 		//tel camara: (11) 3396-4000
 		$lista = $gabineteAoDb->getAll();
+		$jsonArray = array();
+		$i = 0;
 		foreach($lista as $gab){
-			print_r($gab);
+			//print_r($gab);
+			$jsonArray[$i]['gabinete'] = $gab;
 			$vereadorBeanDb = $vereadorAoDb->getById($gab->id_vereador);
-			print_r($vereadorBeanDb);
+			//print_r($vereadorBeanDb);
+			$jsonArray[$i]['vereador'] = $vereadorBeanDb;
 			$vereadorVereancaBeanDb = $vereadorVereancaAoDb->getByIdVereador($vereadorBeanDb->id);
-			print_r($vereadorVereancaBeanDb);
+			//print_r($vereadorVereancaBeanDb);
+			$jsonArray[$i]['vereancas'] = $vereadorVereancaBeanDb;
+			
+			if (!empty($vereadorVereancaBeanDb) 
+				&& !empty($vereadorVereancaBeanDb[0])
+				&& !empty($vereadorVereancaBeanDb[0]->id_vereador_anterior) ){
+				$jsonArray[$i]['vereador_anterior'] = 
+					$vereadorAoDb->getById($vereadorVereancaBeanDb[0]->id_vereador_anterior);
+			}
+			
+			
+			
+			//print_r($jsonArray);
+			$jsonStr .= json_encode($jsonArray)."\n";
+			
+			$i++;
+			
+			//echo $jsonStr;
+			$arrayResult = json_decode($jsonStr);
+			print_r($arrayResult);
 			die();
 		}
 		echo "\nfim\n";
