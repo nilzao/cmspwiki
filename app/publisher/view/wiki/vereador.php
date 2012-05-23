d5fed0bbce6193a5df06bbd4107226ee789856d6
Nilzao <nilzao@gmail.com>
2012-05-23 17:34:52 -0300

    fix conteudo publisher vereador

--- app/publisher/view/wiki/vereador.php
+++ app/publisher/view/wiki/vereador.php
@@ -17,7 +17,7 @@ $gabNum = $gabinete->num_gabinete;
 $gabTel = $gabinete->telefone;
 $gabRamal = $gabinete->ramal;
 $gabFax = $gabinete->fax;
-$gabSala = $gabinete->fax;
+$gabSala = $gabinete->sala;
 ?>
 <div style="float: right;">
 {| align=right style="margin-left: 15px; text-align: center; border:1px solid #ADA268; padding:1px; font-size: 90%; width: 190px;background-color:white" class="box noprint"
@@ -29,22 +29,12 @@ $gabSala = $gabinete->fax;
 <div class="NavFrame" style="border:1px;background-color:#ADA268;padding: 0px;">
 <div style="background-color:#ADA268;border:1px solid #ADA268;padding: 0px 0px 0px 4px; font-size: 105%; text-align:left;margin:1px;">'''<font color="white"><?php echo $situacao;?></font>'''</div></div>
 
-<div class="NavFrame" style="border:0px; background-color:#E0DABF;padding: 0px;">
-<div class="NavHead" style="border:0px; padding: 0px 0px 0px 4px; font-size: 100%; text-align:left;margin:1px;color:black;background-color:#FFF5EE"><b>'''Início [[Mandato]]:
-<?php echo $dataIni;?>'''</b></div>
-</div>
-
 <div class="NavFrame" style="border:0px;background-color:#E0DABF;padding: 1px;">
 <div class="NavHead" style="border:0px solid #FFF5EE;padding: 0px 0px 0px 4px; font-size: 100%; text-align:left;margin:1px;color:black;background-color:#FFF5EE"><b>Partido: [[<?php echo $partido; ?>]]</b></div>
 </div>
 
 <div class="NavFrame" style="border:0px;background-color:#E0DABF;padding: 1px;">
-<div class="NavHead" style="border:0px solid #E0DABF;padding: 0px 0px 0px 4px; font-size: 100%; text-align:left;margin:1px;color:black;background-color:#FFF5EE"><b>Antecessor: [[<?php echo $nomeVereadorAnterior;?>]]</b></div>
-<div class="NavContent" style="display:none;text-align:left;margin:0px 1px;border:none;text-wight:bold; ">
-</div></div>
-
-<div class="NavFrame" style="border:0px;background-color:#E0DABF;padding: 1px;">
-<div class="NavHead" style="border:0px solid #E0DABF;padding: 0px 0px 0px 4px; font-size: 100%; text-align:left;margin:0px;color:black;background-color:#FFF5EE"><b>[[Gabinete]]: [[<?php echo $gabNum; ?>° gabinete]]</b></div>
+<div class="NavHead" style="border:0px solid #E0DABF;padding: 0px 0px 0px 4px; font-size: 100%; text-align:left;margin:0px;color:black;background-color:#FFF5EE"><b>[[<?php echo $gabNum; ?>° gabinete]] - Sala: <?php echo $gabSala;?></b></div>
 <div class="NavContent" style="display:none;text-align:left;margin:0px 1px;border:none;text-wight:bold; ">
 </div></div>
 
@@ -62,29 +52,40 @@ $gabSala = $gabinete->fax;
 |}
 </div>
 == Mandatos ==
-=== 29/03/2011 CMSP===
-<?php echo $nomeVereador; ?>, cumpre o mandato na [[Câmara Municipal de São Paulo]] desde <?php echo $dataIni; ?> Filiado ao [[<?php echo $partido; ?>]] na posição de <?php echo $situacao; ?>.
+=== Vereanças ===
 
-===== Orçamentos para o gabinete =====
+DataIni/DataFim
+"anterior", "posterior", "partido", "obs", etc.
+
+DataIni/DataFim
+"anterior", "posterior", "partido", "obs", etc.
+
+DataIni/DataFim
+"anterior", "posterior", "partido", "obs", etc.
+
+DataIni/DataFim
+"anterior", "posterior", "partido", "obs", etc.
+=== 01/01/2009 a 31/12/2012 CMSP===
+==== Orçamentos para o gabinete ====
 <?php echo $nomeVereador; ?> é o atual reponsável pelo <?php echo $gabNum; ?>º gabinete da [[Câmara Municipal de São Paulo]].
-======Resumo dos [[gastos de gabinete]] no mandato entre 01/2009 a 02/2012 ======
+=====Resumo dos gastos do <?php echo $situacao; ?> =====
+Valores de Janeiro de 2009 até Fevereiro de 2012
 <?php 
 $arrayTitulo = array("'''Tipo de despesa'''"=>"","'''Valor'''"=>"align=\"right\" | R$ "); 
 $arrayNomes = array("descricao","total");
 $tabela = new app_publisher_view_wiki_hlp_Tabela($arrayTitulo, $viewArray->despesas,$arrayNomes);
 echo $tabela->getWikiStr();
 ?>
-O vereador '''<?php echo $nomeVereador; ?>''', é o '''<?php echo $viewArray->ranking_total->posicao; ?>º''' colocado entre os [[vereadores|55 vereadores]] no [[ranking de gastos com gabinete]].
-======Relação de funcionários ligados ao <?php echo $gabNum; ?>º gabinete======
+=====Relação de funcionários ligados ao <?php echo $gabNum; ?>º gabinete=====
 <?php 
 $arrayTitulo = array("'''Nome'''"=>"","'''Cargo'''"=>"");
 $arrayNomes = array("nome","cargo");
 $tabela = new app_publisher_view_wiki_hlp_Tabela($arrayTitulo, $viewArray->funcionarios,$arrayNomes);
 echo $tabela->getWikiStr();
 ?>
-=====Votações=====
+====Votações====
 
-Total de votações nominais em matérias pelo vereador '''<?php echo $nomeVereador; ?>'''
+Total de votações nominais em matérias pelo <?php echo $situacao; ?> '''<?php echo $nomeVereador; ?>'''
 <?php 
 $arrayTitulo = array("'''Tipo de voto'''"=>"","'''Quantidade'''"=>" align=\"right\" | ");
 $arrayNomes = array("tipo_voto","total");
